54eebc311754884b037cb05c8a79f7bcb9620e3e
yeniuma <manczaladri@gmail.com>
2023-02-01 23:21:53 +0100

    added new columns, processed excel names get saved into txt

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import glob
 import os
+import numpy as np
 
 path = "Q:\Projects\DataForFinanceDashboard"
 excel_files = glob.glob(os.path.join(path, "*.xlsx"))
-processed_excels_path = os.path.join(path, "test.txt")
+processed_excels_path = os.path.join(path, "processed_excels.txt")
+
+df = []
 
 if os.path.exists(processed_excels_path):
     with open(processed_excels_path) as f:
@@ -15,5 +18,19 @@ else:
 for f in excel_files:
     if f in processed_excels:
         continue
-    df = pd.read_excel(f)
+    processed_excels.append(f)
+    temp = pd.read_excel(f, index_col=None, header=0)
+    df.append(temp)
 
+frame = pd.concat(df, axis=0, ignore_index=True)
+
+frame['Számla tulajdonos'] = np.where(frame['Számla szám'] == 1177337702327033, 'Adrienn', 'Botond')
+frame['ID'] = frame['Tranzakció dátuma'] + frame['Partner neve'] + str(frame['Számla szám']) + str(frame['Összeg'])
+
+frame.drop_duplicates(subset = "ID")
+
+print(frame)
+
+with open(processed_excels_path, 'w') as f:
+    for item in processed_excels:
+        f.write("%s\n" % item)
