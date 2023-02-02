6b39d7afc9056bc33e6ece0e670df45b44b610a8
yeniuma <manczaladri@gmail.com>
2023-02-02 23:25:57 +0100

    refactor, debugging

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,34 +3,46 @@ import glob
 import os
 import numpy as np
 
+def read_list_of_already_processed_excels(path):
+    if os.path.exists(path):
+        with open(path) as f:
+            t_list = f.read().splitlines()
+    else:
+        t_list = []
+    return(t_list)    
+
+def write_processsed_excels_name(path, processed_excels_list):
+    with open(path, 'w') as f:
+        for item in processed_excels_list:
+            f.write("%s\n" % item)
+
+def read_excels_if_not_already_processed(raw_excels, processed_excels_list):
+    df = []
+    for f in raw_excels:
+        if f in processed_excels_list:
+            continue
+        processed_excels_list.append(f)
+        temp = pd.read_excel(f, index_col=None, header=0)
+        df.append(temp)
+    dframe = pd.concat(df, axis=0, ignore_index=True)
+    return(dframe)
+
 path = "Q:\Projects\DataForFinanceDashboard"
-excel_files = glob.glob(os.path.join(path, "*.xlsx"))
+raw_excel_files = glob.glob(os.path.join(path, "raw\*.xlsx"))
 processed_excels_path = os.path.join(path, "processed_excels.txt")
+compiled_excel_files= os.path.join(path, "clean")
 
-df = []
+processed_excels = read_list_of_already_processed_excels(processed_excels_path)
 
-if os.path.exists(processed_excels_path):
-    with open(processed_excels_path) as f:
-        processed_excels = f.read().splitlines()
-else:
-    processed_excels = []
-
-for f in excel_files:
-    if f in processed_excels:
-        continue
-    processed_excels.append(f)
-    temp = pd.read_excel(f, index_col=None, header=0)
-    df.append(temp)
-
-frame = pd.concat(df, axis=0, ignore_index=True)
+frame = read_excels_if_not_already_processed(raw_excel_files, processed_excels)
 
 frame['Számla tulajdonos'] = np.where(frame['Számla szám'] == 1177337702327033, 'Adrienn', 'Botond')
-frame['ID'] = frame['Tranzakció dátuma'] + frame['Partner neve'] + str(frame['Számla szám']) + str(frame['Összeg'])
+frame['ID'] = frame['Tranzakció dátuma'] + frame['Partner neve'] + frame['Számla szám'].apply(str) + frame['Összeg'].apply(str)
 
 frame.drop_duplicates(subset = "ID")
 
 print(frame)
 
-with open(processed_excels_path, 'w') as f:
-    for item in processed_excels:
-        f.write("%s\n" % item)
+frame.to_excel(os.path.join(compiled_excel_files, "clean_df.xlsx"))
+
+write_processsed_excels_name(processed_excels_path, processed_excels)
