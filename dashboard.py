3de58340a9bc07e9bdfc4a9be3e0490f7323e450
yeniuma <manczaladri@gmail.com>
2023-02-07 21:52:48 +0100

    added date range chooser, implemented dynamic df filtering

--- dashboard.py
+++ dashboard.py
@@ -2,10 +2,36 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import seaborn as sns
+import data_cleaning as dc
+import datetime
 
-path = "Q:\Projects\DataForFinanceDashboard\clean\clean_df.xlsx"
-df = pd.read_excel(path)
 
-st.checkbox("Use container width", value=False, key="use_container_width")
+st.set_page_config(page_title= "Finance dashboard", page_icon = "🦈",layout = "wide")
 
-st.dataframe(df,use_container_width=st.session_state.use_container_width)
+dtchooser1,dtchooser2,fuploader3 = st.columns(3)
+
+with dtchooser1:
+    start_date = st.date_input("Válassz időintervallumot", min_value=datetime.date(2022, 1, 1), max_value=datetime.date.today())
+with dtchooser2:
+    end_date = st.date_input("",min_value=datetime.date(2022, 1, 1), max_value=datetime.date.today())
+with fuploader3:
+    uploaded_file = st.file_uploader("Töltsd fel a tranzakció történetet", type = "xlsx")
+    if uploaded_file is not None:
+       uploaded_df = pd.read_excel(uploaded_file)
+       uploaded_df.to_excel(f"../DataForFinanceDashboard/raw/{uploaded_file.name}")
+
+df = dc.get_clean_data()
+koltsegek = dc.calculate_savings_and_spendings(df,start_date, end_date)
+filtered_df = dc.filter_df_by_date_range(df, start_date, end_date)
+
+metr1, metr2, metr3 = st.columns(3)
+
+st.table(koltsegek)
+
+with st.container():
+    metr1.metric("Bevétel", koltsegek["Bejövő"][0], delta=None, delta_color="normal", help=None, label_visibility="visible")
+    metr2.metric("Költés", koltsegek["Kimenő"][0], delta=None, delta_color="normal", help=None, label_visibility="visible")
+    #col3.metric("Megtakarítás", (koltsegek["Bejövő"][0]-koltsegek["Kimenő"][0]), delta=None, delta_color="normal", help=None, label_visibility="visible")
+    #st.dataframe(df, width = 1920)
+    st.write("Tranzakciótörténet")
+    st.dataframe(filtered_df, use_container_width=True)
--- data_cleaning.py
+++ data_cleaning.py
@@ -2,6 +2,7 @@ import pandas as pd
 import glob
 import os
 import numpy as np
+from datetime import datetime
 
 def read_list_of_already_processed_excels(path):
     if os.path.exists(path):
@@ -9,7 +10,7 @@ def read_list_of_already_processed_excels(path):
             t_list = f.read().splitlines()
     else:
         t_list = []
-    return(t_list)    
+    return t_list     
 
 def write_processsed_excels_name(path, processed_excels_list):
     with open(path, 'w') as f:
@@ -25,26 +26,67 @@ def read_excels_if_not_already_processed(raw_excels, processed_excels_list):
         temp = pd.read_excel(f, index_col=None, header=0)
         df.append(temp)
     dframe = pd.concat(df, axis=0, ignore_index=True)
-    return(dframe)
+    return dframe
 
-path = "Q:\Projects\DataForFinanceDashboard"
-raw_excel_files = glob.glob(os.path.join(path, "raw\*.xlsx"))
-processed_excels_path = os.path.join(path, "processed_excels.txt")
-compiled_excel_files= os.path.join(path, "clean")
+def setup_data_directories():
+    folders = ["DataForFinanceDashboard","DataForFinanceDashboard/raw","DataForFinanceDashboard/clean"]
+    for f in folders:
+        if not os.path.exists(f"../{f}"):
+            os.mkdir(f"../{f}")
 
-processed_excels = read_list_of_already_processed_excels(processed_excels_path)
+    if not os.path.exists("DataForFinanceDashboard/processed_excels.txt"):
+        with open("processed_excels.txt", mode = "w"):
+            pass
 
-frame = read_excels_if_not_already_processed(raw_excel_files, processed_excels)
+def clean_data():
+    setup_data_directories()
 
-frame['Számla tulajdonos'] = np.where(frame['Számla szám'] == 1177337702327033, 'Adrienn', 'Botond')
-frame['ID'] = frame['Tranzakció dátuma'] + frame['Partner neve'] + frame['Számla szám'].apply(str) + frame['Összeg'].apply(str)
+    path = "..\DataForFinanceDashboard"
+    raw_excel_files = glob.glob(os.path.join(path, "raw\*.xlsx"))
+    processed_excels_path = os.path.join(path, "processed_excels.txt")
+    compiled_excel_files= os.path.join(path, "clean")
 
-frame.drop_duplicates(subset = "ID")
+    processed_excels = read_list_of_already_processed_excels(processed_excels_path)
 
-frame.drop(columns=frame.columns[-1], inplace=True)
+    frame = read_excels_if_not_already_processed(raw_excel_files, processed_excels)
 
-print(frame)
+    frame['Számla tulajdonos'] = np.where(frame['Számla szám'] == 1177337702327033, 'Adrienn', 'Botond')
+    frame['ID'] = frame['Tranzakció dátuma'] + frame['Partner neve'] + frame['Számla szám'].apply(str) + frame['Összeg'].apply(str)
 
-frame.to_excel(os.path.join(compiled_excel_files, "clean_df.xlsx"), index=False)
+    frame.drop_duplicates(subset = "ID")
 
-write_processsed_excels_name(processed_excels_path, processed_excels)
+    frame.drop(columns=["ID", "Számla név", "Számla szám", "Partner számlaszáma/azonosítója"], inplace=True)
+
+    frame.to_excel(os.path.join(compiled_excel_files, f"clean_df{datetime.now()}.xlsx"), index=False)
+
+    write_processsed_excels_name(processed_excels_path, processed_excels)
+
+def get_clean_data():
+    try:
+        path = "..\DataForFinanceDashboard\clean\clean_df.xlsx"
+        df = pd.read_excel(path)
+    except FileNotFoundError:
+        return pd.DataFrame()
+    return df
+
+def calculate_savings_and_spendings(df, start_date, end_date):
+    df['Tranzakció dátuma'] = pd.to_datetime(df['Tranzakció dátuma'], errors='coerce')
+    df['Könyvelés dátuma'] = pd.to_datetime(df['Tranzakció dátuma']).dt.date
+
+    koltsegek = df[(df['Könyvelés dátuma']>=start_date) & (df['Könyvelés dátuma']<=end_date)] 
+
+    koltsegek = koltsegek.groupby(["Bejövő/Kimenő", df['Tranzakció dátuma'].dt.year.rename('Év'), df['Tranzakció dátuma'].dt.month_name().rename('Hónap')])["Összeg"].sum().reset_index()
+    koltsegek = pd.pivot_table(koltsegek, values = "Összeg", columns= "Bejövő/Kimenő", index = ["Év", "Hónap"]).reset_index()
+    koltsegek["Bejövő"] = koltsegek.get('Bejövő', 0)  
+    koltsegek["Kimenő"] = koltsegek.get('Kimenő', 0)  
+    koltsegek.loc[:, "Bejövő"], koltsegek.loc[:, "Kimenő"] = koltsegek["Bejövő"].map('{:,d}'.format), koltsegek["Kimenő"].map('{:,d}'.format)
+
+    return koltsegek
+
+def filter_df_by_date_range(df, start_date, end_date):
+    df['Könyvelés dátuma'] = pd.to_datetime(df['Tranzakció dátuma']).dt.date
+    filtered_df = df[(df['Könyvelés dátuma']>=start_date) & (df['Könyvelés dátuma']<=end_date)]
+    filtered_df.drop(columns= "Könyvelés dátuma", inplace= True)
+    filtered_df['Tranzakció dátuma'] = filtered_df['Tranzakció dátuma'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'))
+
+    return filtered_df
