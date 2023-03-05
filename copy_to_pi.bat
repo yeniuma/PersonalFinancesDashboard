82ce90a094178474d8f13436907582fbf91483ad
Holdmedve <talos.boti@gmail.com>
2023-03-05 19:26:35 +0100

    🌐 Add copy to pi bat file

---
+++ copy_to_pi.bat
@@ -0,0 +1,9 @@
+scp ^
+    auth.py ^
+    data_cleaning.py ^
+    main.py ^
+    setup.py ^
+    auth.yaml ^
+    Dockerfile ^
+    requirements.txt ^
+    computah@192.168.1.135:/home/computah/PersonalFinancesDashboard
