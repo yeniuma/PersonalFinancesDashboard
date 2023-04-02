83f165cbbf0ffb12ac6b79bfbaaa26f14edeeb66
Holdmedve <talos.boti@gmail.com>
2023-04-02 18:27:00 +0200

    ⚰️ Remove unused bat file

--- copy_to_pi.bat
+++
@@ -1,8 +0,0 @@
-scp ^
-    auth.py ^
-    data_cleaning.py ^
-    main.py ^
-    setup.py ^
-    auth.yaml ^
-    requirements.txt ^
-    holdmedve@192.168.1.166:/home/holdmedve/PersonalFinancesDashboard
